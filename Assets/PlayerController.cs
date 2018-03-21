cb1b8c324a8dc1b126a26bab2aa18987cd79939b
henvas <henrivas@stud.ntnu.no>
2018-03-21 15:28:53 +0100

    Player navMesh

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Mouse: ");
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Make path
+                Debug.Log("path");
+                agent.SetDestination(hit.point);
+            }
+        }
+	}
+}
