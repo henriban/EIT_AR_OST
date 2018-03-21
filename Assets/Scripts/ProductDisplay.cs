1d8f6afa1b78f81163c3ed1aef42e70ef5a930e5
Henrik Bjelke Anderson <henriban@stud.ntnu.no>
2018-03-21 13:09:07 +0100

    Added productCard prefab

--- Assets/Scripts/ProductDisplay.cs
+++ Assets/Scripts/ProductDisplay.cs
@@ -6,14 +6,19 @@ public class ProductDisplay : MonoBehaviour {
     public ProductModel product;
 
     public Text productName;
+    public Text productInfo;
     public Image productImage;
     public Text productPrice;
-    public Text productInfo;
+    public Text productInStore;
+    public Text productDescription;
 
     void Start() {
         productName.text = product.productName;
-        productImage.sprite = product.productImage;
-        productPrice.text = product.productPrice;
         productInfo.text = product.productInfo;
+        productImage.sprite = product.productImage;
+        productPrice.text = product.productPrice + ",- / stk";
+        productInStore.text = product.inStore ? "På lager" : "Ikke på lager";
+        productDescription.text = product.productDescription;
+        
     }
 }
