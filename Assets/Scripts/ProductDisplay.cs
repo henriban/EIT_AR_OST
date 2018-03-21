0d898e65dff90328de75c613cec896f487be4ead
Henrik Bjelke Anderson <henriban@stud.ntnu.no>
2018-03-21 11:00:45 +0100

    Added productscripts and one product

---
+++ Assets/Scripts/ProductDisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ProductDisplay : MonoBehaviour {
+
+    public ProductModel product;
+
+    public Text productName;
+    public Image productImage;
+    public Text productPrice;
+    public Text productInfo;
+
+    void Start() {
+        productName.text = product.productName;
+        productImage.sprite = product.productImage;
+        productPrice.text = product.productPrice;
+        productInfo.text = product.productInfo;
+    }
+}
---
+++ Assets/Scripts/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Product", menuName = "Product")]
+public class ProductModel : ScriptableObject {
+
+    public string productName;
+    public string productInfo;
+    public string productPrice;
+    public Sprite productImage;
+    public string productDescription;
+    public bool inStore;
+
+}
