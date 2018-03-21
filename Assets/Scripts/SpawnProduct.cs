b7cb3fdd6fd0b5655c42279fdc522e6ce1046295
Henrik Bjelke Anderson <henriban@stud.ntnu.no>
2018-03-21 14:45:49 +0100

    Added productSpawn, ARCamera with LookAtScript

---
+++ Assets/Scripts/SpawnProduct.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SpawnProduct : MonoBehaviour {
+
+    public GameObject product1;
+    public GameObject product2;
+
+    public GameObject imageTarget;
+
+    void Start() {
+
+        GameObject newObject = Instantiate(product1, new Vector3(0 , 0, 0), Quaternion.identity);
+        //Instantiate(product2, new Vector3(1, 1, 1), Quaternion.identity);
+
+        //newObject.transform.parent = imageTarget.transform;
+        newObject.transform.SetParent(imageTarget.transform);
+    }
+}
