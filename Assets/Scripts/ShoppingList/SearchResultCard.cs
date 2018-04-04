bddcde0c6baa5559e3d928895cbd942f5913756c
Henrik Bjelke Anderson <henriban@stud.ntnu.no>
2018-04-04 15:50:38 +0200

    Added ShoppingList GUI and display scripts

---
+++ Assets/Scripts/ShoppingList/SearchResultCard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SearchResultCard : MonoBehaviour {
+
+    public ProductModel product;
+
+    public Image productImage;
+    public Text productNameText;
+
+    //public Button addButton;
+
+    void Start() {
+
+        productImage.sprite = product.productImage;
+        productNameText.text = product.productName;
+
+    }
+}
---
+++ Assets/Scripts/ShoppingList/ShoppingListItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShoppingListItem : MonoBehaviour {
+
+    public ProductModel product;
+
+    public Image porductImage;
+    public Text productNameText;
+
+    public Button removeButton;
+
+    void Start () {
+
+        porductImage.sprite = product.productImage;
+        productNameText.text = product.productName;
+
+        removeButton.onClick.AddListener(HandleClick);
+	}
+
+    private void HandleClick() {
+        Debug.Log("Remove ");
+    }
+}
