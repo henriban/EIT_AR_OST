f78e2c576c4ca90112d3f85dcb264f36f72a8572
Simen <simennj@outlook.com>
2018-04-17 15:44:18 +0200

    add aisleview

---
+++ Assets/ImageChanger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ImageChanger : MonoBehaviour
+{
+    public Texture[] Textures;
+    private int _textureIndex = 0; // Use this for initialization
+    private Slider _slider;
+
+    void Start()
+    {
+        _slider = gameObject.GetComponent<Slider>();
+        _slider.maxValue = Textures.Length;
+        _slider.onValueChanged.AddListener(setTexture);
+    }
+
+    void setTexture(float index)
+    {
+        RenderSettings.skybox.SetTexture("_MainTex", Textures[(int) index - 1]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && _textureIndex < Textures.Length - 1)
+        {
+            RenderSettings.skybox.SetTexture("_MainTex", Textures[++_textureIndex]);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && _textureIndex > 0)
+        {
+            RenderSettings.skybox.SetTexture("_MainTex", Textures[--_textureIndex]);
+        }
+    }
+}
