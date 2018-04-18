c3d8aab9f0b59ddfee693b14c823dee5e5ec14fc
henvas <henrivas@stud.ntnu.no>
2018-04-18 14:52:20 +0200

    workingslidemeny

--- Assets/Scripts/UI_ManagerScript.cs
+++ Assets/Scripts/UI_ManagerScript.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_ManagerScript : MonoBehaviour {
+
+    private GameObject hamburgerButton;
+
+    private void Start()
+    {
+        hamburgerButton = GameObject.Find("SidemenyButton");
+    }
 
 	public void DisableBoolAnimator(Animator anim)
     {
         anim.SetBool("IsDisplayed", false);
+        hamburgerButton.SetActive(true);
     }
 
     public void EnableBoolAnimator(Animator anim)
     {
         anim.SetBool("IsDisplayed", true);
+        hamburgerButton.SetActive(false);
     }
 
 }
