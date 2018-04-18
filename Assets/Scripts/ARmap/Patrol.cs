e81e6e6d672029dbfc162ecf33758b716dc7fd4f
Simen <simennj@outlook.com>
2018-04-18 21:39:46 +0200

    fix pathfinding in ArMap

--- Assets/Scripts/ARmap/Patrol.cs
+++ Assets/Scripts/ARmap/Patrol.cs
@@ -1,142 +1,77 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
-using System.Collections;
-using System.Collections.Generic;
 
 public class Patrol : MonoBehaviour
 {
-    public GameObject Cube;
     public GameObject Player;
-    public GameObject imagetarget;
-    public GameObject startPos;
-    public GameObject endPos;
-    public float countdown = 0.030f;
+    public GameObject Products;
+    public GameObject StartPos;
+    public GameObject EndPos;
 
-    private List<Transform> points;
-    private Transform[] poeng;
-    private int destPoint = 0;
-    private NavMeshAgent agent;
-    private bool done = false;
+    private List<Vector3> _poeng;
+    private int _destPoint;
+    private NavMeshAgent _agent;
+    private bool _done;
+
+    public GameObject[] Goals;
 
     void Start()
     {
-        points = new List<Transform>();
-        points.Add(startPos.transform);
-        //int index = 1;
-        foreach (Product product in Data.AllProducts)
-            // foreach (Product product in Data.ShoppingList)
+        _poeng = new List<Vector3> {StartPos.transform.position};
+        foreach (Product product in Data.AllProducts) //.ShoppingList)
         {
-            points.Add(product.productPosition.transform);
-            //index++;
+            var position = Products.transform.Find(product.productPosition.name).position;
+            _poeng.Add(position);
+//            _poeng.Add(product.productPosition.transform);
         }
+//        foreach (var goal in Goals)
+//        {
+//            _poeng.Add(goal.transform);
+//        }
 
+        _poeng.Add(EndPos.transform.position);
 
-        points.Add(endPos.transform);
-        int count = 0;
-        foreach (Transform t in points)
-        // foreach (Product product in Data.ShoppingList)
-        {
-            count++;
-            Debug.Log(t.position+"for index"+ count);
-            
-            //index++;
-        }
-
-        poeng = points.ToArray();
-        Debug.Log(poeng);
-        agent = GetComponent<NavMeshAgent>();
-
-        // Disabling auto-braking allows for continuous movement
-        // between points (ie, the agent doesn't slow down as it
-        // approaches a destination point).
-        agent.autoBraking = false;
-
-        //GotoNextPoint();
+        _agent = GetComponent<NavMeshAgent>();
     }
 
- 
+
     bool GotoNextPoint()
     {
-        // Returns if no points have been set up
-        if (poeng.Length == 0)
+        if (_destPoint < _poeng.Count)
         {
+            Debug.Log(_poeng[_destPoint]);
+            _agent.destination = _poeng[_destPoint++];
+
+            Debug.Log("Agent dest" + _agent.destination);
             return false;
-        }
-        if (destPoint < poeng.Length)
-        {
-            agent.destination = poeng[destPoint].position;
-            if (agent.destination != poeng[destPoint].position)
-            {
-                agent.destination = poeng[destPoint].position;
-            }
-            Debug.Log("Agent dest" + agent.destination);
-            Debug.Log("Point position" + poeng[destPoint].position + "at destPoint " + destPoint);
-            destPoint += 1;
-            return false;
-        }
-        else
-        {
-            //Debug.Log("We are done");
-            return true;
-        }
-        /*
-        if (points.Count == 0)
-        {
-            return false;
-        }
-        if (destPoint < points.Count)
-        {
-            agent.destination = points[destPoint].position;
-            if (agent.destination != points[destPoint].position)
-            {
-                agent.destination = points[destPoint].position;
-            }
-            Debug.Log("Agent dest"+agent.destination);
-            Debug.Log("Point position" + points[destPoint].position +"at destPoint "+ destPoint);
-            destPoint += 1;
-            return false;
-        } else
-        {
-            Debug.Log("We are done");
-            return true;
         }
 
-        // Set the agent to go to the currently selected destination.
-        
-
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
-        //destPoint = (destPoint + 1) % points.Length;
-        */
+        Debug.Log("We are done");
+        return true;
     }
 
 
+    public Transform Dots;
     void Update()
     {
-        // Choose the next destination point when the agent gets
-        // close to the current one.
-        countdown -= Time.deltaTime;
-        if (countdown <= 0.0f)
+        if (_poeng.Count == 0)
         {
-            //Debug.Log(done);
-            //Debug.Log(destPoint);
-            if (done == false)
-            {
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                cube.transform.position = Player.transform.position;
-                cube.transform.SetParent(imagetarget.gameObject.transform);
-                cube.transform.rotation = Player.transform.rotation;
-                Vector3 scale = new Vector3(0.01f, 0.005f, 0.01f);
-                cube.transform.localScale = scale;
-                cube.GetComponent<Renderer>().material.color = Color.green;
-            }
-            if (!agent.pathPending && agent.remainingDistance < 0.5f)
-                if (GotoNextPoint())
-                {
-                    //GotoNextPoint();
-                    done = true;
+            _done = true;
+        }
+        if (_done == false)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            cube.transform.SetParent(Dots);
+            cube.transform.localPosition = Player.transform.position;
+            cube.transform.localScale = new Vector3(0.5f, 0.25f, 0.5f);
+            cube.transform.localRotation = Quaternion.identity;
+            cube.GetComponent<Renderer>().material.color = Color.green;
+        }
 
-                }
+        if (!_agent.pathPending && _agent.remainingDistance < _agent.stoppingDistance)
+        {
+            _done = GotoNextPoint();
         }
     }
 }
