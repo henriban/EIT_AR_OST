527f232e41773e3439b3e1233796b9ca268226c8
henvas <henrivas@stud.ntnu.no>
2018-04-18 13:40:17 +0200

    working slide menu

---
+++ Assets/Scripts/UI_ManagerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ManagerScript : MonoBehaviour {
+
+	public void DisableBoolAnimator(Animator anim)
+    {
+        anim.SetBool("IsDisplayed", false);
+    }
+
+    public void EnableBoolAnimator(Animator anim)
+    {
+        anim.SetBool("IsDisplayed", true);
+    }
+
+}
