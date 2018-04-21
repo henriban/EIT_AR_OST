22e3d387e53f25651e766e25c92267fbbd6a6650
Henrik Bjelke Anderson <henriban@stud.ntnu.no>
2018-04-21 13:13:51 +0200

    Product information on productCard/ListItem click

--- Assets/Scripts/ShoppingList/SearchResultCard.cs
+++ Assets/Scripts/ShoppingList/SearchResultCard.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class SearchResultCard : MonoBehaviour {
 
+    public GameObject productInformationPanel;
+    private GameObject canvas;
+
     private Product product;
 
     public Image productImage;
@@ -14,16 +17,25 @@ public class SearchResultCard : MonoBehaviour {
 
     void Start() {
 
-        addButton.onClick.AddListener(HandleClick);
-    }
+        canvas = GameObject.Find("Canvas");
 
+        addButton.onClick.AddListener(HandleClick);
+        gameObject.transform.GetComponent<Button>().onClick.AddListener(HandleProductInformationClick);
+    }
 
     public void Setup(Product currentProduct) {
 
-        product = currentProduct;        
+        product = currentProduct;
 
         productImage.sprite = product.productImage;
         productNameText.text = product.productName;
+    }
+
+    private void HandleProductInformationClick() {
+        
+        productInformationPanel = Instantiate(productInformationPanel);
+        productInformationPanel.transform.SetParent(canvas.transform, false);
+        productInformationPanel.GetComponent<ProductInformationDisplay>().Setup(product);
     }
 
     private void HandleClick() {
--- Assets/Scripts/ShoppingList/ShoppingListItem.cs
+++ Assets/Scripts/ShoppingList/ShoppingListItem.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ShoppingListItem : MonoBehaviour {
@@ -6,14 +7,28 @@ public class ShoppingListItem : MonoBehaviour {
     public Image porductImage;
     public Text productNameText;
 
+    public GameObject productInformationPanel;
+    private GameObject canvas;
+
     public Button removeButton;
 
     private Product product;
 
     void Start () {
-        
+
+        canvas = GameObject.Find("Canvas");
+
         removeButton.onClick.AddListener(HandleClick);
+        gameObject.transform.GetComponent<Button>().onClick.AddListener(HandleProductInformationClick);
+
 	}
+
+    private void HandleProductInformationClick() {
+        
+        productInformationPanel = Instantiate(productInformationPanel);
+        productInformationPanel.transform.SetParent(canvas.transform, false);
+        productInformationPanel.GetComponent<ProductInformationDisplay>().Setup(product);
+    }
 
     public void Setup(Product currentProduct) {
 
